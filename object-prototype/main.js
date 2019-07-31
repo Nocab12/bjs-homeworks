8b67e9666c03c185369127b550fd5b6206ab5234
Denis Berezin <nocab@yandex.ru>
2019-08-01 00:04:01 +0300

    homework 5.0

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,6 +8,12 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+    let now = Date.now();
+    let userBirthday = new Date (birthday).getTime();
+    let diff = now - userBirthday;
+    let age = ( diff / ( (31622400000+(31536000000*3)) /4) );
+    console.log(age);
+    return (age>18);
 }
 
 function initPrintAnimalSound() {
@@ -22,7 +28,15 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
-}
+    this.animal = animal;
+    let sound = animal.sound;
+
+    if (!animal) {
+        return null
+        } else if (animal) { 
+        return sound;
+    }
+};
 
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
@@ -36,4 +50,9 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+    let sum = 0;
+    for (let i = 0; i < marks.length; i++) {
+      sum += parseInt(marks[i], 10);
+    }
+    return Math.round(sum / marks.length);
 }
